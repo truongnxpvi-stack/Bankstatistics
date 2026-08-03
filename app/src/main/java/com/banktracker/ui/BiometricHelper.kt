76f4be106b3252d444d545051578a4e21301830c
truongnxpvi-stack <truongnx.pvi@gmail.com>
2026-08-03 15:14:38 +0700

    Create BiometricHelper.kt

---
+++ app/src/main/java/com/banktracker/ui/BiometricHelper.kt
@@ -0,0 +1,34 @@
+package com.banktracker.ui
+
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+
+object BiometricHelper {
+    fun canAuthenticate(activity: FragmentActivity): Boolean {
+        val mgr = BiometricManager.from(activity)
+        return mgr.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS
+    }
+    fun showPrompt(activity: FragmentActivity, onSuccess: () -> Unit, onFailed: () -> Unit, onError: (String) -> Unit) {
+        val executor = ContextCompat.getMainExecutor(activity)
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) { onSuccess() }
+            override fun onAuthenticationFailed() { onFailed() }
+            override fun onAuthenticationError(code: Int, msg: CharSequence) {
+                if (code == BiometricPrompt.ERROR_NEGATIVE_BUTTON || code == BiometricPrompt.ERROR_USER_CANCELED)
+                    activity.finish()
+                else onError(msg.toString())
+            }
+        }
+        val prompt = BiometricPrompt(activity, executor, callback)
+        val info = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("BankTracker")
+            .setSubtitle("Xác thực để truy cập")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+        prompt.authenticate(info)
+    }
+}
