e3d2c321e4e03148e493def48344f4daa7fa1aa1
truongnxpvi-stack <truongnx.pvi@gmail.com>
2026-08-03 15:14:16 +0700

    Create BankNotifListenerService.kt

---
+++ app/src/main/java/com/banktracker/service/BankNotifListenerService.kt
@@ -0,0 +1,27 @@
+package com.banktracker.service
+
+import android.app.Notification
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import com.banktracker.data.AppDatabase
+import com.banktracker.parser.BankSmsParser
+
+class BankNotifListenerService : NotificationListenerService() {
+    private val BANK_APPS = setOf(
+        "com.VCB","com.mbmobile","com.techcombank.mb.app",
+        "vn.com.tpb.smartbanking","com.vnpay.vpbankonline",
+        "com.vietinbank.ipay","com.agribank.mobile"
+    )
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        if (sbn.packageName !in BANK_APPS) return
+        val extras = sbn.notification.extras
+        val title  = extras.getString(Notification.EXTRA_TITLE) ?: ""
+        val text   = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString() ?: ""
+        val full   = "$title $text"
+        if (BankSmsParser.looksLikeTransaction(full)) {
+            BankSmsParser.parse(sbn.packageName, full)?.let { tx ->
+                AppDatabase.getInstance(applicationContext).transactionDao().insert(tx)
+            }
+        }
+    }
+}
